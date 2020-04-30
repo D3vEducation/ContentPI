4c5122db077c0d6ab28f21b2179aff435ca9a044
@telecristy <telematicacristina@gmail.com>
2020-04-29 17:15:01 -0700

    Adding pictures logic

--- frontend/src/app/dashboard/components/Media/index.tsx
+++ frontend/src/app/dashboard/components/Media/index.tsx
@@ -1,5 +1,6 @@
 // Dependencies
 import React, { FC, ReactElement, memo, useState, useEffect } from 'react'
+import { cx } from 'fogg-utils'
 
 // Shared components
 import MainLayout from '@layouts/main/MainLayout'
@@ -39,6 +40,14 @@ const Media: FC = (): ReactElement => {
     }
   ]
 
+  // Data for pictures
+  const pictureFiles = [
+    {
+      name: 'Picture 1',
+      imgUrl: 'http://localhost:3000/images/logo.png'
+    }
+  ]
+
   // Local state
   const [carousel, setCarousel] = useState(false)
   const [modeGrid, setModeGrid] = useState(false)
@@ -53,23 +62,23 @@ const Media: FC = (): ReactElement => {
   // Methods
   // Method for render files
   const renderFiles = (data: any): ReactElement => {
-    return data.map((document: any, i: any) => {
-      const extension = document.name.substr(document.name.lastIndexOf('.') + 1)
+    return data.map((item: any, i: any) => {
+      const extension = item.name.substr(item.name.lastIndexOf('.') + 1)
 
       return (
-        <section key={`file-${i}`}>
+        <section key={`file-${i}`} className={styles.item}>
           {extension === 'doc' && <i className="fas fa-file-word" style={{ color: '#0061a7' }} />}
           {extension === 'pdf' && <i className="fas fa-file-pdf" style={{ color: '#f30000' }} />}
           {extension === 'xml' && <i className="fas fa-file-excel" style={{ color: '#3b6e22' }} />}
 
-          <span>{document.name}</span>
+          {item.imgUrl ? <img src={item.imgUrl} alt={item.name} /> : <span>{item.name}</span>}
         </section>
       )
     })
   }
 
   // Method to handle mode grid or carousel
-  const handleMode = (mode: any) => {
+  const handleMode = (mode: any): any => {
     if (documentsFiles.length >= 5) {
       if (mode === 'grid') {
         setCarousel(false)
@@ -88,15 +97,14 @@ const Media: FC = (): ReactElement => {
           My documents
           <i
             className={`far fa-square ${styles.modeCarousel} ${!modeGrid ? styles.disabled : ''}`}
-            onClick={() => handleMode('carousel')}
+            onClick={(): void => handleMode('carousel')}
           />
           <i
             className={`fas fa-border-all ${styles.modeGrid} ${!carousel ? styles.disabled : ''}`}
-            onClick={() => handleMode('grid')}
+            onClick={(): void => handleMode('grid')}
           />
         </div>
         <div className={styles.line} />
-
         <section className={styles.filesContainer}>
           {carousel ? (
             <section className={styles.carousel}>
@@ -109,6 +117,14 @@ const Media: FC = (): ReactElement => {
               {renderFiles(documentsFiles)}
             </section>
           )}
+        </section>
+
+        <div className={styles.title}>My Pictures</div>
+        <div className={styles.line} />
+        <section className={styles.filesContainer}>
+          <section className={cx(styles.files, styles.modeGrid)}>
+            {renderFiles(pictureFiles)}
+          </section>
         </section>
       </section>
     </MainLayout>
