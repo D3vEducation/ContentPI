e84148da338e98901d7cc99509dc5c5bdb99dd4c
Carlos Santana <carlos.santana@dev.education>
2020-06-17 18:59:18 -0700

    V36 - Update Entry (Part 2)

--- frontend/src/app/dashboard/components/Content/Create/index.tsx
+++ frontend/src/app/dashboard/components/Content/Create/index.tsx
@@ -1,13 +1,9 @@
 // Dependencies
 import React, { FC, ReactElement, useState, useContext, memo } from 'react'
-import { Alert, Badge, Icon, Input, TextArea, PrimaryButton, SuccessButton } from 'fogg-ui'
-import { cx, slugFn, getEmptyValues, waitFor } from 'fogg-utils'
+import { slugFn, getEmptyValues, waitFor } from 'fogg-utils'
 import { v4 as uuidv4 } from 'uuid'
 import moment from 'moment'
 import { useMutation } from '@apollo/react-hooks'
-
-// Constants
-import { CONTENT_LINK } from '@constants/links'
 
 // Shared components
 import MainLayout from '@layouts/main/MainLayout'
@@ -20,20 +16,21 @@ import { FormContext } from '@contexts/form'
 import CREATE_VALUES_MUTATION from '@graphql/values/createValues.mutation'
 import FIND_UNIQUE_VALUES_MUTATION from '@graphql/values/findUniqueValues.mutation'
 
-// Styles
-import styles from './Create.scss'
+// Components
+import CustomFields from '../CustomFields'
+import SystemFields from '../SystemFields'
 
 interface iProps {
   router: any
   data: any
 }
 
-// Setting a unique ID
-const newId = uuidv4()
-
 const Create: FC<iProps> = ({ data, router }): ReactElement => {
   // Data
   const { getModel } = data
+
+  // Setting a unique ID
+  const newId = uuidv4()
 
   // Fields
   const initialValues: any = {}
@@ -56,10 +53,6 @@ const Create: FC<iProps> = ({ data, router }): ReactElement => {
   systemFields.forEach((field: any) => {
     let value = field.defaultValue
 
-    if (field.identifier === 'id') {
-      value = newId
-    }
-
     if (field.identifier === 'createdAt') {
       value = moment().format('MM/DD/YYYY hh:mm a')
     }
@@ -68,7 +61,9 @@ const Create: FC<iProps> = ({ data, router }): ReactElement => {
       value = ''
     }
 
-    systemInitialValues[field.identifier] = value
+    if (field.identifier !== 'id') {
+      systemInitialValues[field.identifier] = value
+    }
   })
 
   // States
@@ -81,6 +76,7 @@ const Create: FC<iProps> = ({ data, router }): ReactElement => {
   const [required, setRequired] = useState(requiredValues)
   const [saveLoading, setSaveLoading] = useState(false)
   const [publishLoading, setPublishLoading] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
   // Mutations
   const [createValuesMutation] = useMutation(CREATE_VALUES_MUTATION)
@@ -90,6 +86,8 @@ const Create: FC<iProps> = ({ data, router }): ReactElement => {
   const { onChange, setValue } = useContext(FormContext)
 
   // Methods
+  const handleAfterCreateOrEditEntryModal = (): void => setIsOpen(!isOpen)
+
   const handleActive = (field: string) => {
     setActive(field)
   }
@@ -180,8 +178,9 @@ const Create: FC<iProps> = ({ data, router }): ReactElement => {
               status: values.status
             })
 
-            waitFor(2).then(() => {
+            waitFor(1).then(() => {
               setShowAlert(false)
+              handleAfterCreateOrEditEntryModal()
             })
           }
         }
@@ -191,130 +190,31 @@ const Create: FC<iProps> = ({ data, router }): ReactElement => {
 
   return (
     <MainLayout title="Create new Entry" header content footer sidebar noWrapper router={router}>
-      <div className={styles.create}>
-        <div className={styles.fields}>
-          <div className={styles.goBack}>
-            <Link href={CONTENT_LINK(router).as} title={`Go back to ${getModel.modelName}`}>
-              <Icon type="fas fa-chevron-left" />
-            </Link>
-            &nbsp;&nbsp;&nbsp;
-            <Badge className={styles.badge}>{getModel.modelName}</Badge>
-            {values.title ? <>{values.title}</> : <>New {getModel.modelName}</>}
-          </div>
+      <>
+        <CustomFields
+          active={active}
+          customFields={customFields}
+          getModel={getModel}
+          handleActive={handleActive}
+          onChange={_onChange}
+          required={required}
+          router={router}
+          values={values}
+        />
 
-          {customFields.map((field: any) => (
-            <div
-              key={field.id}
-              className={cx(
-                styles.field,
-                active === field.identifier ? styles.active : '',
-                required[field.identifier] ? styles.red : ''
-              )}
-              onClick={(): void => handleActive(field.identifier)}
-            >
-              <div>
-                <label>
-                  {field.fieldName}{' '}
-                  {field.isRequired && (
-                    <span className={cx(styles.tag, required[field.identifier] ? styles.red : '')}>
-                      Required
-                    </span>
-                  )}
-                </label>
-              </div>
-
-              {field.type === 'String' && (
-                <div className={styles[field.type.toLowerCase()]}>
-                  <Input
-                    type="text"
-                    hasError={required[field.identifier]}
-                    name={field.identifier}
-                    onChange={_onChange}
-                    placeholder={field.fieldName}
-                    value={values[field.identifier]}
-                  />
-                </div>
-              )}
-
-              {field.type === 'Text' && (
-                <div className={styles[field.type.toLowerCase()]}>
-                  <TextArea
-                    name={field.identifier}
-                    hasError={required[field.identifier]}
-                    placeholder={field.fieldName}
-                    onChange={_onChange}
-                  />
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-
-        <div className={styles.sidebar}>
-          <div className={styles.wrapper}>
-            <div className={styles.block}>Status</div>
-
-            <div className={styles.row}>
-              <PrimaryButton
-                onClick={(): any => handleSubmit('save')}
-                isLoading={saveLoading}
-                disabled={publishLoading}
-                loadingText="Saving..."
-              >
-                Save
-              </PrimaryButton>
-              <SuccessButton
-                onClick={(): any => handleSubmit('publish')}
-                isLoading={publishLoading}
-                disabled={saveLoading}
-                loadingText="Publishing..."
-              >
-                Publish
-              </SuccessButton>
-            </div>
-
-            <div className={styles.block}>System Fields</div>
-
-            <div className={styles.row}>
-              <div className={styles.systemField}>
-                <div>ID</div>
-                <div className={styles.id}>{newId}</div>
-              </div>
-
-              {systemFields.map((systemField: any): any => {
-                if (systemField.identifier !== 'id') {
-                  return (
-                    <div key={systemField.id} className={styles.systemField}>
-                      <div>
-                        {systemField.identifier === 'updatedAt'
-                          ? 'Last saved'
-                          : systemField.fieldName}
-                      </div>
-                      <div
-                        className={cx(
-                          styles[systemField.identifier],
-                          styles[systemValues[systemField.identifier].toLowerCase()],
-                          systemValues[systemField.identifier] === '' ? styles.empty : ''
-                        )}
-                      >
-                        {systemValues[systemField.identifier]}
-                      </div>
-                    </div>
-                  )
-                }
-
-                return <div key={systemField.id} />
-              })}
-            </div>
-          </div>
-
-          <div className={cx(styles.alert, showAlert ? styles.show : '')}>
-            <Alert success={alertType === 'success'} danger={alertType === 'danger'} flat>
-              {alert}
-            </Alert>
-          </div>
-        </div>
-      </div>
+        <SystemFields
+          alert={alert}
+          alertType={alertType}
+          handleSubmit={handleSubmit}
+          isOpen={isOpen}
+          publishLoading={publishLoading}
+          router={router}
+          saveLoading={saveLoading}
+          showAlert={showAlert}
+          systemFields={systemFields}
+          systemValues={systemValues}
+        />
+      </>
     </MainLayout>
   )
 }
